862d93cf20e0e1e015ebf37e00fbcaa25eb50372
khanzele16 <khanzele16@gmail.com>
2025-04-20 15:14:49 +0300

    Added widgets

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Home from "@/components/Home";
 
 export default function Page() {
   return (
-    <div className=''>
+    <div className='flex justify-center content-center items-center'>
       <Home />
     </div>
   )
--- components/Widgets/index.tsx
+++ components/Widgets/index.tsx
@@ -1,16 +1,16 @@
-import { WalletMinimal } from "lucide-react";
+import { KeyRound, WalletMinimal } from "lucide-react";
 import React from "react";
 
 export function Widgets() {
   return (
-    <div className="w-full relative max-w-[500px] h-[250px] p-6 flex flex-row justify-between items-center content-center gap-x-[20px] sm:mx-[100px]">
-      <div className="w-full flex justify-center content-center items-center gap-x-[6px] rounded-xl p-4 bg-[#898989]">
-        <WalletMinimal color="black" size={24} />
-        <p className="text-black">Пополнить баланс</p>
+    <div className="w-full flex flex-row justify-between items-center content-center gap-x-[20px] sm:mx-[100px]">
+      <div className="w-full flex justify-center content-center items-center gap-x-[8px] rounded-xl p-4 shadow-md bg-[#fff] border border-solid border-[#c2c2c2]">
+        <WalletMinimal color="black" size={25} />
+        <p className="text-lg text-black">Пополнить баланс</p>
       </div>
-      <div className="w-full flex justify-center content-center items-center gap-x-[6px] rounded-xl p-4 bg-[#898989]">
-        <WalletMinimal color="black" size={24} />
-        <p className="text-black">Пополнить баланс</p>
+      <div className="w-full flex justify-center content-center items-center gap-x-[8px] rounded-xl p-4 shadow-md bg-[#fff] border border-solid border-[#c2c2c2]">
+        <KeyRound color="black" size={25} />
+        <p className="text-lg text-black">Мои ключи</p>
       </div>
     </div>
   );
