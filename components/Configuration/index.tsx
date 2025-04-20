5939ad66a5195a82763ffcdc39ed210da2d06ef3
webddevel <unar0448@gmail.com>
2025-04-20 16:11:30 +0300

    Revert

--- components/Configuration/index.tsx
+++ components/Configuration/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 
-export function index() {
-  return (
-    <div className=''>
-        <h1>Моя конфигурация</h1>
-    </div>
-  )
+export function Configuration() {
+    return (
+        <div className='w-full max-w-[500px] flex m-auto flex-col'>
+            <div className='w-full flex justify-between text-black'>
+                <h1 className='text-[#000000]'>Моя конфигурация</h1>
+                <p className='text-[#AAAAAA]'>Все</p>
+            </div>
+            <div className="bg-[#151515] rounded-[5px] mt-[12px]">
+                <div className='w-full flex justify-between text-black max-w-[450px] m-auto py-[10px]'>
+                    <h1 className='text-[#fff]'>Локация</h1>
+                    <p className='text-[#fff]'>Протокол</p>
+                </div>
+                <div className="">
+                    
+                </div>
+            </div>
+        </div>
+
+    )
 }
 
--- components/Home/index.tsx
+++ components/Home/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Card } from '../Card'
 import { Widgets } from '../Widgets'
+import { Configuration } from '../Configuration'
 
 function Home() {
   return (
     <div className='w-full gap-y-[25px] max-w-[500px] flex flex-col justify-center items-center content-center font-[family-name:var(--font-inter)]'>
       <Card />
       <Widgets />
+      <Configuration/>
     </div>
   )
 }
